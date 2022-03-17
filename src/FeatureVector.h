773f2cdd18f72fe3e0580dcdde3d0241aa61e173
qiuzhichang <qiu_zhi_chang@126.com>
2022-03-17 23:12:48 +0800

    format files

--- src/FeatureVector.h
+++ src/FeatureVector.h
@@ -11,45 +11,41 @@
 #define __D_T_FEATURE_VECTOR__
 
 #include "BowVector.h"
+#include "exports.h"
+
 #include <map>
 #include <vector>
-#include "exports.h"
 namespace DBoW3 {
 
 /// Vector of nodes with indexes of local features
-class  DBOW_API FeatureVector:
-  public std::map<NodeId, std::vector<unsigned int> >
-{
-public:
+class DBOW_API FeatureVector : public std::map<NodeId, std::vector<unsigned int>> {
+  public:
+    /**
+     * Constructor
+     */
+    FeatureVector(void);
 
-  /**
-   * Constructor
-   */
-  FeatureVector(void);
-  
-  /**
-   * Destructor
-   */
-  ~FeatureVector(void);
-  
-  /**
-   * Adds a feature to an existing node, or adds a new node with an initial
-   * feature
-   * @param id node id to add or to modify
-   * @param i_feature index of feature to add to the given node
-   */
-  void addFeature(NodeId id, unsigned int i_feature);
+    /**
+     * Destructor
+     */
+    ~FeatureVector(void);
 
-  /**
-   * Sends a string versions of the feature vector through the stream
-   * @param out stream
-   * @param v feature vector
-   */
-  friend std::ostream& operator<<(std::ostream &out, const FeatureVector &v);
-    
+    /**
+     * Adds a feature to an existing node, or adds a new node with an initial
+     * feature
+     * @param id node id to add or to modify
+     * @param i_feature index of feature to add to the given node
+     */
+    void addFeature(NodeId id, unsigned int i_feature);
+
+    /**
+     * Sends a string versions of the feature vector through the stream
+     * @param out stream
+     * @param v feature vector
+     */
+    friend std::ostream& operator<<(std::ostream& out, const FeatureVector& v);
 };
 
-} // namespace DBoW3
+}  // namespace DBoW3
 
 #endif
-
